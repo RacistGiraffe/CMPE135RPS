1a718f4f4e9f7cd3b789d6f485b5bb3dea7edd95
Zachary Nguyen <znguyenmdc30@gmail.com>
2020-03-14 01:59:24 -0700

    Revert "More cleanup"
    This reverts commit 4fb614ff37ac89128c77420903006ad0a84ede75.

--- src/src/Computer.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "../include/Computer.h"
-using namespace std;
-
-unsigned int Computer::getcomputerselection() {
-	return computerselection;
-}
--- src/src/RandomComputer.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "../include/Computer.h"
-#include "../include/RandomComputer.h"
-using namespace std;
-
-void RandomComputer::setcomputerselection() {
-	computerselection = (rand() % 3) + 1;
-}
